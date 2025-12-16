3b6ec666b1d8dcf63ee5a00ca6815c2985eee44a
Azka rizqullah <azkarizqullah99@gmail.com>
2025-12-16 13:12:49 +0700

    feat: initial commit

---
+++ synchronization/time_sync/node_a/run.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+python3 peer_node.py \
+	  --name A --listen 0.0.0.0 5000 \
+	  --peers B@192.168.122.162:5000 C@192.168.122.216:5000 D@192.168.122.94:5000 \
+	  --logger 192.168.122.236 9999 \
+	  --offset-ms 600 \
+	  --initiate-broadcast --msg "Hello from A"
+
