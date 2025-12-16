520d911482c9b0f5221b6fa4f1022a0ff09b6c2c
Azka rizqullah <azkarizqullah99@gmail.com>
2025-12-16 13:34:52 +0700

    fix: path

--- synchronization/time_sync/node_a/run.bash
+++ synchronization/time_sync/node_a/run.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 peer_node.py \
+python3 ../program/peer_node.py \
 	  --name A --listen 0.0.0.0 5000 \
 	  --peers B@192.168.122.162:5000 C@192.168.122.216:5000 D@192.168.122.94:5000 \
 	  --logger 192.168.122.236 9999 \
--- synchronization/time_sync/node_b/run.bash
+++ synchronization/time_sync/node_b/run.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 peer_node.py \
+python3 ../program/peer_node.py \
 	  --name B --listen 0.0.0.0 5000 \
 	  --peers A@192.168.122.49:5000 C@192.168.122.216:5000 D@192.168.122.94:5000 \
 	  --logger 192.168.122.236 9999 \
